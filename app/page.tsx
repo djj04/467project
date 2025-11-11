1d9a85c89ef023f2014e970522158af533bc9872
djj04 <dougjj04@gmail.com>
2025-11-11 17:17:55 -0600

    saving changes before merge

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,18 @@ import Navbar from '@/components/Navbar';
 import PartsCard from '@/components/PartsCard';
 
 export default function Home() {
+  const testamount = 24;
+  const testprice = 28.95;
+
   return (
+    <>
     <Navbar />
+    <PartsCard name="testname"
+               desc="this is a test description for this auto part!" 
+               image="/testimg1.jfif" 
+               amount={testamount}
+               price={testprice}
+      />
+    </>
   );
 }
--- components/PartsCard.tsx
+++ components/PartsCard.tsx
@@ -11,7 +11,16 @@ type CardProps = {
 export default function PartsCard({name, desc, image, price, amount}: CardProps) {
     return (
         <div className={styles.card}>
-            
+            <img className={styles.cardimg} src={image} alt={name}/>
+            <h2 className={styles.name}>{name}</h2>
+            <p className={styles.desc}>{desc}</p>
+            <div className={styles.priceamtlayout}>
+                <p>Price: ${price}</p>
+                <p>Quantity: {amount}</p>
+            </div>
+            <div className={styles.cart}>
+                <button>Add to Cart</button>
+            </div>
         </div>
     )
 }
