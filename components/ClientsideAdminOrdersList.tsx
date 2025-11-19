a25b481083d58eb51787fb25aa87be51e21a7a25
zoe <zoe@niu.edu>
2025-11-18 19:39:13 -0600

    Allow filtering orders on price in admin page

--- components/ClientsideAdminOrdersList.tsx
+++ components/ClientsideAdminOrdersList.tsx
@@ -6,6 +6,9 @@ import AdminOrderCard from "./AdminOrderCard";
 export default function ClientsideAdminOrdersList({orders}: {orders: any[]}) {
 	let [filterIncludesAuthorized, setFilterIncludesAuthorized] = useState(true)
 	let [filterIncludesShipped, setFilterIncludesShipped] = useState(true)
+
+	let [minimumPrice, setMinimumPrice] = useState(-99999999.99)
+	let [maximumPrice, setMaximumPrice] = useState(99999999.99)
 	
 	return (
 		<div>
@@ -17,12 +20,24 @@ export default function ClientsideAdminOrdersList({orders}: {orders: any[]}) {
 					<br />
 					<input type="checkbox" id="shipped" checked={filterIncludesShipped} onChange={e=>setFilterIncludesShipped(e.target.checked)} /> <label htmlFor="shipped">shipped</label>
 				</div>
+				<div>
+					<h4>Price</h4>
+					<label htmlFor="minimum-price">From: </label>
+					<input type="number" step="any" id="minimum-price" value={minimumPrice} onChange={e=>setMinimumPrice(parseFloat(e.target.value))} />
+					<label htmlFor="maximum-price"> to: </label>
+					<input type="number" step="any" id="maximum-price" value={maximumPrice} onChange={e=>setMaximumPrice(parseFloat(e.target.value))} />
+				</div>
 			</div>
 			<ul>
 				{orders
 					.filter(order => (
-						(filterIncludesAuthorized && order.order.status == "authorized") ||
-						(filterIncludesShipped && order.order.status == "shipped")
+						(
+							(filterIncludesAuthorized && order.order.status == "authorized") ||
+							(filterIncludesShipped && order.order.status == "shipped")
+						) && (
+							order.order.totalPriceCharged >= minimumPrice &&
+							order.order.totalPriceCharged <= maximumPrice
+						)
 					))
 					.map(order=>(
 					<li key={order.order.id}>
