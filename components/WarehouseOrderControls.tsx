bfb0ed576e241964fab75a68001d80aef0712d4a
zoe <zoe@niu.edu>
2025-11-18 20:37:17 -0600

    Add way to print invoice for orders

--- components/WarehouseOrderControls.tsx
+++ components/WarehouseOrderControls.tsx
@@ -1,8 +1,8 @@
 "use client"
 
 export default function WarehouseOrderControls({orderID}: {orderID: number}) {
-	const printPackingList = () => {
-		const listWindow = window.open("/warehousePackingList?orderID=" + encodeURIComponent(orderID))
+	const print = (baseURL: string) => {
+		const listWindow = window.open(baseURL + "?orderID=" + encodeURIComponent(orderID))
 		if (!listWindow) {
 			alert("Cannot print, maybe unblock popups?")
 			return
@@ -12,10 +12,14 @@ export default function WarehouseOrderControls({orderID}: {orderID: number}) {
 			listWindow.close()
 		}
 	}
+
+	const printPackingList = print.bind(null, "/warehousePackingList")
+	const printInvoice = print.bind(null, "/warehouseInvoice")
 	
 	return (
 		<div>
 			<button onClick={printPackingList}>Print packing list</button>
+			<button onClick={printInvoice}>Print invoice</button>
 		</div>
 	)
 }
