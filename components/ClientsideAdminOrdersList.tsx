e8ff8bccf9d35631a18284e83d001e13686e4555
zoe <zoe@niu.edu>
2025-11-18 19:35:42 -0600

    Allow filtering orders on status in admin page

--- components/ClientsideAdminOrdersList.tsx
+++ components/ClientsideAdminOrdersList.tsx
@@ -1,17 +1,37 @@
 "use client"
 
+import { useState } from "react";
 import AdminOrderCard from "./AdminOrderCard";
 
 export default function ClientsideAdminOrdersList({orders}: {orders: any[]}) {
+	let [filterIncludesAuthorized, setFilterIncludesAuthorized] = useState(true)
+	let [filterIncludesShipped, setFilterIncludesShipped] = useState(true)
+	
 	return (
-		<ul>
-            {orders.map(order=>(
-                <li key={order.order.id}>
-                    <AdminOrderCard
-                        order={order}
-                    />
-                </li>
-            ))}
-        </ul>
+		<div>
+			<div>
+				<h3>Filter</h3>
+				<div>
+					<h4>Status</h4>
+					<input type="checkbox" id="authorized" checked={filterIncludesAuthorized} onChange={e=>setFilterIncludesAuthorized(e.target.checked)} /> <label htmlFor="authorized">authorized</label>
+					<br />
+					<input type="checkbox" id="shipped" checked={filterIncludesShipped} onChange={e=>setFilterIncludesShipped(e.target.checked)} /> <label htmlFor="shipped">shipped</label>
+				</div>
+			</div>
+			<ul>
+				{orders
+					.filter(order => (
+						(filterIncludesAuthorized && order.order.status == "authorized") ||
+						(filterIncludesShipped && order.order.status == "shipped")
+					))
+					.map(order=>(
+					<li key={order.order.id}>
+						<AdminOrderCard
+							order={order}
+						/>
+					</li>
+				))}
+			</ul>
+		</div>
 	)
 }
