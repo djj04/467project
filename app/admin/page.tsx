b6594101b1d1ca4f7b24543d342c4cc54a0881c5
zoe <zoe@niu.edu>
2025-11-21 15:47:56 -0600

    Ensure /admin and /warehouse are always dynamically rendered

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,15 @@
 import Navbar from '@/components/Navbar';
 import AdminInterface from '@/components/AdminInterface';
 
-export default async function Admin() {
+export default async function Admin(props: {
+		searchParams?: Promise<{
+			orderID?: string;
+		}>;
+}) {
+
+	// This is just so that the page is NOT statically built with `npm run build`. There is seemingly no documentation suggesting a better way to do this??
+	await props.searchParams
+	
   return (
 	<>
 	<Navbar />
--- app/warehouse/page.tsx
+++ app/warehouse/page.tsx
@@ -1,7 +1,14 @@
 import Navbar from '@/components/Navbar';
 import WarehouseShipmentInterface from '@/components/WarehouseShipmentInterface';
 
-export default function Warehouse() {
+export default async function Warehouse(props: {
+		searchParams?: Promise<{
+			orderID?: string;
+		}>;
+}) {
+
+	// This is just so that the page is NOT statically built with `npm run build`. There is seemingly no documentation suggesting a better way to do this??
+	await props.searchParams
   return (
 	<>
 	<Navbar />
