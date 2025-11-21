c335afba5b3e025ec102318059819ace24a426d5
zoe <zoe@niu.edu>
2025-11-20 20:31:02 -0600

    Fix insertion of ship dates

--- lib/db.ts
+++ lib/db.ts
@@ -306,7 +306,7 @@ export class Order {
                     await item.part.addInventory(-1 * item.quantity)
                 }
             }
-            await New.query("UPDATE orders SET status=\"shipped\", date_shipped=? WHERE id=?", [(new Date()).toISOString(), this.id])
+            await New.query("UPDATE orders SET status=\"shipped\", date_shipped=STR_TO_DATE(?, '%Y-%m-%dT%H:%i:%s.%fZ') WHERE id=?", [(new Date()).toISOString(), this.id])
             await New.query("COMMIT")
         } catch (error) {
             console.error("Error in finalizing order!", this, error)
