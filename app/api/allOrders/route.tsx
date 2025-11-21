e9db8de76e2bba9398596cf69d9996d51ccfa835
zoe <zoe@niu.edu>
2025-11-21 14:34:13 -0600

    Print errors from /api/allOrders to server console

--- app/api/allOrders/route.tsx
+++ app/api/allOrders/route.tsx
@@ -5,6 +5,7 @@ export async function GET(): Promise<Response> {
 		const orders = await Order.listWithItems()
 		return new Response(JSON.stringify(orders), {status: 200})
 	} catch (error) {
+		console.error(error)
 		return new Response("", { status: 500 })
 	}
 }
