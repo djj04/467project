9089b3c27f79632b0a28fcd425f113b3af06b6e9
TDananGH <niutykalel21@gmail.com>
2025-11-23 11:09:35 -0600

    Update route.ts

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,1 +1,39 @@
+import { NextResponse } from "next/server"
+import { Part } from "@/lib/db"
 
+export async function GET(request: Request)
+{
+    const { searchParams } = new URL(request.url)
+    const query = searchParams.get("query") || ""
+
+    try
+    {
+        let res: Part[] = [] 
+
+        //Search By Part Number 
+        const numQ = parseInt(query)
+        if (!isNaN(numQ))
+        {
+            const part = await Part.byNumber(numQ)
+            if (part)
+            {
+                res.push(part)
+            }
+        }
+
+        //Search By Part Name
+        const allParts = await Part.list(0)
+        if (allParts)
+        {
+            const filtered = allParts.filter(p => p.description.toLowerCase().includes(query.toLowerCase()))
+            res = res.concat(filtered); 
+        }
+
+        return NextResponse.json(res);
+    }
+
+    catch (error)
+    {
+        return NextResponse.json([], { status: 500 })
+    }
+}
