dd3a1ffd62415eb61236955f361f8f9ba21c533b
zoe <zoe@niu.edu>
2025-11-25 19:04:43 -0600

    Add icons to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,4 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Link from 'next/link';
 import styles from './Navbar.module.css';
 
@@ -8,19 +9,19 @@ export default function Navbar() {
       <h1>Welcome to our Product Website!</h1>
       <ul className={styles.menu}>
         <li>
-          <Link href="/">Home</Link>
+          <Link href="/"><FontAwesomeIcon icon="house" width="1em" />&nbsp;Home</Link>
         </li>
         <li>
-          <Link href="/cart">Cart</Link>
+          <Link href="/cart"><FontAwesomeIcon icon="basket-shopping" width="1em" />&nbsp;Cart</Link>
         </li>
         <li>
-          <Link href="/receiving">Receiving</Link>
+          <Link href="/receiving"><FontAwesomeIcon icon="warehouse" width="1em" />&nbsp;Receiving</Link>
         </li>
         <li>
-          <Link href="/admin">Admin</Link>
+          <Link href="/admin"><FontAwesomeIcon icon="person" width="1em" />&nbsp;Admin</Link>
         </li>
         <li>
-          <Link href="/warehouse">Warehouse</Link>
+          <Link href="/warehouse"><FontAwesomeIcon icon="box" width="1em" />&nbsp;Warehouse</Link>
         </li>
       </ul>
     </nav>
