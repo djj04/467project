513ee602d3095db52fe20525f824115c8f6efca6
TDananGH <niutykalel21@gmail.com>
2025-11-30 17:47:33 -0600

    Create PartToggle.tsx

---
+++ components/PartToggle.tsx
@@ -0,0 +1,56 @@
+"use client"
+
+import { useState, useEffect } from "react"
+import SearchItem from "./SearchItem"
+import PartsCard from "./PartsCard"
+import PartsListPaginationNav from "./PartsListPaginationNav"
+import { Part } from "@/lib/db"
+import styles from "./PartsList.module.css"
+
+type ToggleProps = {
+  pageNumber: number
+}
+
+export default function PartToggle({ pageNumber }: ToggleProps) {
+  const [results, setResults] = useState<Part[]>([])
+  const [searching, setSearch] = useState(false)
+  const [list, setList] = useState<Part[]>([])
+
+  useEffect(() => {
+    const getList = async () => {
+      const res = await fetch("/api/search?query=")
+      const data: Part[] = await res.json()
+      setList(data)
+    }
+    getList()
+  }, [])
+
+  const handleSearch = (searchResults: Part[]) => {
+    setResults(searchResults)
+    setSearch(true)
+  }
+
+  const showParts = searching ? results : list
+
+
+  return (
+    <>
+      <SearchItem onResults={handleSearch} />
+
+      {showParts.length > 0 ? (
+        <ul className={styles.items}>
+          {showParts.map((part) => (
+            <li key={part.number}>
+              <PartsCard part={part}/>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p> No parts found</p>
+      )}
+
+      {!searching && <PartsListPaginationNav pageNumber={pageNumber}/>}
+
+    </>
+  )
+}
